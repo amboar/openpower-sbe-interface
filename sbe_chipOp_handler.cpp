d6165c6e97a15e3d575945f2a11ffd6883182d27
Andrew Jeffery <andrew@aj.id.au>
2018-03-21 15:22:29 +1030

    sbe_chipOp_handler: Improve read behaviour
    Prepare to read until we have read all data, and in the mean time, ensure
    we've prepared a read buffer at least as long as the expected response.
    
    Note that the actual response may also be either smaller or greater in
    size than the expected response[1]. Currently there's no proper way to
    handle these cases as the SBEFIFO driver issues EAGAIN to a read() when
    all data has been read instead of returning 0. As a consequence we have
    no correct way to extract all the data from the FIFO, as the only means
    to tell we've read all the data is for a short read to occur. Given the
    read syscall interface allows for short reads to occur normally (i.e. a
    short read does not imply all data has been read) we reach an ambiguous
    state. We can't rely on EAGAIN for similar reasons.
    
    At the moment, we assume all data has been read on a short read.
    
    Further, improve the error handling by adding description tables for the
    primary and secondary status error codes. This improves the output of
    applications using the library.
    
    [1] If the request is not correctly formulated then an error status will
    be returned with no data preceding the metadata. If an error occurred
    whilst the request was being service then we may receive FFDC data in
    addition to the data and metadata already provided.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- sbe_chipOp_handler.cpp
+++ sbe_chipOp_handler.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <array>
 #include <errno.h>
 #include <fcntl.h>
@@ -6,6 +7,7 @@
 #include <endian.h>
 #include <sbe_chipOp_handler.hpp>
 #include <file.hpp>
+#include <iostream>
 namespace openpower
 {
 namespace sbe
@@ -26,9 +28,10 @@ std::vector<sbe_word_t> writeToFifo(const char* devPath,
                                     size_t cmdBufLen,
                                     size_t respBufLen)
 {
-    size_t len = 0;
     std::vector<sbe_word_t> response;
     std::ostringstream errMsg;
+    ssize_t len;
+    void *pos;
 
     //Open the device and obtain the file descriptor associated with it.
     FileDescriptor fileFd(devPath, (O_RDWR | O_NONBLOCK));
@@ -82,32 +85,40 @@ std::vector<sbe_word_t> writeToFifo(const char* devPath,
         throw std::runtime_error(errMsg.str().c_str());
     }
     printf("Poll for read completed\n");
-    //Derive the total read length which should include the FFDC, which SBE
-    //returns in case of failure.
-    size_t totalReadLen = respBufLen + MAX_FFDC_LEN_IN_WORDS;
     //Create a temporary buffer
-    std::vector<sbe_word_t> buffer(totalReadLen);
+    std::vector<sbe_word_t> buffer(respBufLen);
 printf("Will attempt to read\n");
-    auto bytesToRead = (totalReadLen * WORD_SIZE);
-    len = read(fileFd(), buffer.data(), bytesToRead);
-    if (len < 0)
+
+    len = 0;
+    size_t readSize;
+    size_t bytesRead = 0;
+    do {
+        if (len > 0)
+        {
+            bytesRead += len;
+            // Manually handle the vector expansion as we're writing to the
+            // data store in read(), and we need data() to be big enough
+            // already.
+            buffer.resize(2 * buffer.size());
+        }
+        pos = &(buffer.data()[bytesRead]);
+        readSize = buffer.capacity() * sizeof(sbe_word_t) - bytesRead;
+        errno = 0;
+
+    } while ((len = read(fileFd(), pos, readSize)) > 0);
+
+    if (errno != EAGAIN)
     {
         //TODO:use elog infrastructure
         errMsg << "Failed to read the FIFO device:" << devPath << "bytes read ="
-               << len << " errno=" << errno;
-        throw std::runtime_error(errMsg.str().c_str());
-    }
-    else if (len != bytesToRead)
-    {
-        //TODO:use elog infrastructure
-        errMsg << "Exepcted " << bytesToRead << " bytes to be read from FIFO"
-               " device:" << devPath << ", but bytes read are " << len <<
-               " errno= " << errno;
+            << len << " errno=" << errno;
         throw std::runtime_error(errMsg.str().c_str());
     }
 
+    buffer.resize(bytesRead / sizeof(sbe_word_t));
+
     //Extract the valid number of words read.
-    for (auto i = 0; i < (rc / WORD_SIZE); ++i)
+    for (size_t i = 0; i < buffer.size(); i++)
     {
         response.push_back(be32toh(buffer[i]));
     }
@@ -117,6 +128,60 @@ printf("Will attempt to read\n");
     return response;
 }
 
+static const char* primaryStatusDesc[] =
+{
+    [0x0000] = "Operation successful",
+    [0x0001] = "Invalid or unsupported command",
+    [0x0002] = "Invalid data passed",
+    [0x0003] = "Sequence Error",
+    [0x0004] = "SBE Internal Error",
+};
+
+static const char* secondaryStatusDesc[] =
+{
+    [0x0000] = "Operation successful",
+    [0x0001] = "Command Class not supported",
+    [0x0002] = "Command not supported",
+    [0x0003] = "Invalid address passed",
+    [0x0004] = "Invalid Target Type passed",
+    [0x0005] = "Invalid Chiplet Id passed",
+    [0x0006] = "Target not present",
+    [0x0007] = "Target is not functional",
+    [0x0008] = "Command not allowed in current state",
+    [0x0009] = "Functionality not Supported",
+    [0x000A] = "Generic failure in execution",
+    [0x000B] = "Blacklisted registers or memory accessed",
+    [0x000C] = "SBE Operating System Failure",
+    [0x000D] = "SBE FIFO Access Failure",
+    [0x000E] = "Insufficient data passed as part of command",
+    [0x000F] = "Excess data passed as part of command",
+    [0x0010] = "Hardware timeout",
+    [0x0011] = "PCB-PIB Error",
+    [0x0012] = "SBE FIFO Parity Error",
+};
+
+static const char* getPrimaryStatusDesc(const uint16_t primary)
+{
+    if (primary < (sizeof(primaryStatusDesc) / sizeof(*primaryStatusDesc)))
+    {
+        return primaryStatusDesc[primary];
+    }
+
+    assert(primary == 0x00FE);
+
+    return "Unknown Error";
+}
+
+static const char* getSecondaryStatusDesc(const uint16_t secondary)
+{
+    if (secondary < (sizeof(secondaryStatusDesc) / sizeof(*secondaryStatusDesc)))
+    {
+        return secondaryStatusDesc[secondary];
+    }
+
+    return "Undefined error code";
+}
+
 void parseResponse(std::vector<sbe_word_t>& sbeDataBuf)
 {
     //Number of 32-bit words obtained from the SBE
@@ -124,7 +189,7 @@ void parseResponse(std::vector<sbe_word_t>& sbeDataBuf)
 
     //Fetch the SBE header and SBE chiop primary and secondary status
     //Last value in the buffer will have the offset for the SBE header
-    size_t distanceToStatusHeader =  sbeDataBuf[sbeDataBuf.size() - 1];
+    size_t distanceToStatusHeader = sbeDataBuf[sbeDataBuf.size() - 1];
 
     if (lengthObtained < distanceToStatusHeader)
     {
@@ -147,7 +212,9 @@ void parseResponse(std::vector<sbe_word_t>& sbeDataBuf)
 
     //Fetch the primary and secondary response code
     std::advance(iter, DISTANCE_TO_RESP_CODE);
-    auto l_priSecResp = *iter;
+    sbe_word_t l_status = *iter;
+    const uint16_t primary = (l_status >> 16) & 0xffff;
+    const uint16_t secondary = l_status & 0xffff;
 
     //Validate the magic code obtained in the response
     if (l_magicCode != MAGIC_CODE)
@@ -160,7 +227,7 @@ void parseResponse(std::vector<sbe_word_t>& sbeDataBuf)
     }
 
     //Validate the Primary and Secondary response value
-    if (l_priSecResp != SBE_OPERATION_SUCCESSFUL)
+    if (l_status != SBE_OPERATION_SUCCESSFUL)
     {
         //Extract the SBE FFDC and throw it to the caller
         size_t ffdcLen = (distanceToStatusHeader -
@@ -179,8 +246,11 @@ void parseResponse(std::vector<sbe_word_t>& sbeDataBuf)
         //TODO:use elog infrastructure to return the SBE and Hardware procedure
         //FFDC container back to the caller.
         std::ostringstream errMsg;
-        errMsg << "Chip operation failed with SBE response code:" << l_priSecResp
-               << ".Length of FFDC data of obtained:" << ffdcLen;
+        errMsg << "Chip operation failed with SBE response code "
+               << std::hex << primary << ":" << std::hex << secondary
+               << ": " << getPrimaryStatusDesc(primary) << ", "
+               << getSecondaryStatusDesc(secondary)
+               << ". Length of FFDC data of obtained: " << ffdcLen;
         throw std::runtime_error(errMsg.str().c_str());
     }
 
